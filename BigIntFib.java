c66bc0102d2eadf9f20f99e562ed1854bc286da9
snehamandan <mandansneha@gmail.com>
2017-11-17 05:39:11 +0530

    Update BigIntFib.java

--- BigIntFib.java
+++ BigIntFib.java
@@ -3,49 +3,58 @@ import java.lang.*;
 import java.math.BigInteger;
 import java.io.*;
 
-public class BigIntFib {
-
-	static BigInteger mod=new BigInteger("1000000007");
-    static BigInteger MAX =new BigInteger("100000000000000");
-    static BigInteger f[];
-     
-    // Returns n'th fibonacci number using 
-    // table f[]
-    public static BigInteger fib(BigInteger bigInteger)
-    {
-        // Base cases
-        if (bigInteger.equals(BigInteger.valueOf(0)))
-            return BigInteger.valueOf(1);
-             
-        if (bigInteger.equals(BigInteger.valueOf(1))|| bigInteger.equals(BigInteger.valueOf(2)))
-            return (f[bigInteger.intValue()] = BigInteger.valueOf(1));
-      
-        // If fib(n) is already computed
-        if (!f[bigInteger.intValue()].equals(BigInteger.valueOf(0)))
-            return f[bigInteger.intValue()];
-      
-        BigInteger k = (bigInteger.and(BigInteger.valueOf(1))).equals(BigInteger.valueOf(1))? bigInteger.add(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2))
-                            : bigInteger.divide(BigInteger.valueOf(2));
-      
-        // Applyting above formula [Note value
-        // n&1 is 1 if n is odd, else 0.
-        f[bigInteger.intValue()] = (bigInteger.and(BigInteger.valueOf(1))).equals(BigInteger.valueOf(1))? (fib(k).multiply(fib(k)).add( 
-                        fib(k.subtract(BigInteger.valueOf(1))).multiply(fib(k.subtract(BigInteger.valueOf(1))))))
-                       : (fib(k.subtract(BigInteger.valueOf(1))).multiply(BigInteger.valueOf(2)).add(fib(k))).multiply(fib(k));
-      
-        return f[bigInteger.intValue()].mod(mod);
-    }
-     
-    /* Driver program to test above function */
-    public static void main(String[] args) 
-    {
-      Scanner s=new Scanner(System.in);
-  	  int t=s.nextInt();
-  	  f= new BigInteger[MAX.intValue()];
-  	  for(int i=0;i<t;i++){
-  		  BigInteger n=s.nextBigInteger();
-  		  System.out.println(fib(n.add(BigInteger.valueOf(2))));
-  	  }
-        
-    }
+public class Solution {
+	static BigInteger m=BigInteger.valueOf(1000000007);
+  static BigInteger fib(BigInteger n)
+  {
+	  BigInteger F[][] = new BigInteger[][]{{BigInteger.ONE,BigInteger.ONE},{BigInteger.ONE,BigInteger.ZERO}};
+  if (n.equals(BigInteger.ZERO))
+      return BigInteger.ZERO;
+  power(F, n.subtract(BigInteger.ONE));
+    
+  return F[0][0].mod(m);
+  }
+    
+  static void multiply(BigInteger[][] f, BigInteger[][] f2)
+  {
+	  BigInteger x =  (f[0][0].multiply(f2[0][0])).add(f[0][1].multiply(f2[1][0]));
+	  
+	  BigInteger y =  (f[0][0].multiply(f2[0][1])).add(f[0][1].multiply(f2[1][1]));
+	  
+	  BigInteger z =  (f[1][0].multiply(f2[0][0])).add(f[1][1].multiply(f2[1][0]));
+	  
+	  BigInteger w =  (f[1][0].multiply(f2[0][1])).add(f[1][1].multiply(f2[1][1]));
+   
+  f[0][0] = x.mod(m);
+  f[0][1] = y.mod(m);
+  f[1][0] = z.mod(m);
+  f[1][1] = w.mod(m);
+  }
+    
+  static void power(BigInteger[][] f, BigInteger bigInteger)
+  {
+  if( bigInteger.equals(BigInteger.ZERO) || bigInteger.equals(BigInteger.ONE))
+    return;
+  BigInteger M[][] = new BigInteger[][]{{BigInteger.ONE,BigInteger.ONE},{BigInteger.ONE,BigInteger.ZERO}};
+    
+  power(f, bigInteger.divide(BigInteger.valueOf(2)));
+  multiply(f, f);
+    
+  if (!bigInteger.remainder(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0)))
+     multiply(f, M);
+  }
+   
+  public static void main (String args[])
+  {
+	  
+	  Scanner s=new Scanner(System.in);
+	  int t=s.nextInt();
+	  for(int i=0;i<t;i++){
+		  BigInteger n=s.nextBigInteger();
+		  //Required shifted Fibonacci series
+		  System.out.println(fib(n.add(BigInteger.valueOf(2))));
+	  }
+       
+   
+  }
 }
